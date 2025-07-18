c14df041c0aed3e1c69b818ad56d597074155066
Muhammad Ahsan <ahsanwebengr@gmail.com>
2025-07-18 12:36:26 +0500

    feat: update the middleware checks using utils classes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,22 +1,20 @@
 import jwt from 'jsonwebtoken';
 import { MESSAGES, STATUS_CODES } from '../constants/index.js';
 import Users from '../models/user.model.js';
-import { asyncHandler, handleError } from '../utils/index.js';
+import { asyncHandler, checkField, handleError } from '../utils/index.js';
 import { ACCESS_TOKEN_SECRET } from '../configs/env.config.js';
 
 export const verifyJWT = (roles = []) =>
   asyncHandler(async (req, _, next) => {
     try {
       const token = req.headers.authorization?.split(' ')[1];
-      if (!token) {
-        handleError(next, STATUS_CODES.UNAUTHORIZED, MESSAGES.UNAUTHORIZED);
-      }
+
+      checkField(!token, MESSAGES.UNAUTHORIZED, STATUS_CODES.UNAUTHORIZED);
       const decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
       const user = await Users.findById(decodedToken?._id).select('-password');
 
-      if (!user) {
-        handleError(next, STATUS_CODES.UNAUTHORIZED, MESSAGES.UNAUTHORIZED);
-      }
+      checkField(!user, MESSAGES.UNAUTHORIZED, STATUS_CODES.UNAUTHORIZED);
+
       if (roles.length && !roles.includes(user.role)) {
         return handleError(next, STATUS_CODES.FORBIDDEN, MESSAGES.FORBIDDEN);
       }
